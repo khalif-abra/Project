9a7b7951e982b1c432e42b16f026a05b26634815
abra <almostlipe@gmail.com>
2024-06-27 16:28:38 +0700

    add

--- resources/views/Layouts/main.blade.php
+++ resources/views/Layouts/main.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-        <link rel="stylesheet" type="text/css" href="http://blog-khalif.uni-app.my.id/css/donation.css">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
         <link href="https://unpkg.com/aos@next/dist/aos.css" rel="stylesheet">
         <title>{{ $title }}</title>
@@ -21,10 +21,10 @@
 
 
         <!-- Scripts -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+    <script src="/https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+VV0A5ZWy5xKt4KGS2l9KKUtiKV8y" crossorigin="anonymous">
 </script>
-<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+<script src="/https://unpkg.com/aos@next/dist/aos.js"></script>
 <script>
 
 </script>
