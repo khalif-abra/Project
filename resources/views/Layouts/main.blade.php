d623c2eca2f652d4ba43f71379f22b50906c6176
abra <almostlipe@gmail.com>
2024-06-27 15:46:50 +0700

    add

--- resources/views/Layouts/main.blade.php
+++ resources/views/Layouts/main.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-        <link rel="stylesheet" href="/css/donation.css">
+        <link rel="stylesheet" type="text/css" href="http://blog-khalif.uni-app.my.id/css/donation.css">
         <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
         <link href="https://unpkg.com/aos@next/dist/aos.css" rel="stylesheet">
         <title>{{ $title }}</title>
