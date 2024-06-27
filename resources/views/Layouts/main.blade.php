4b31b93bbf7565dbe0606720b16514d3f8ab9ae7
abra <almostlipe@gmail.com>
2024-06-27 16:30:14 +0700

    penambahan css styling

--- resources/views/Layouts/main.blade.php
+++ resources/views/Layouts/main.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/Donation.css') }}">
         <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
         <link href="https://unpkg.com/aos@next/dist/aos.css" rel="stylesheet">
         <title>{{ $title }}</title>
